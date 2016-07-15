2e27481d47d9d3fe8651fd5d17acdf2e5a4027b5
Bubbleinpit <coke909@live.com>
2016-07-15 10:33:36 +0800

    Revert "Add change passwd acctivity"
    This reverts commit 55cf470ffe8095325bbabd162c81089c922608a6.

--- app/src/main/java/com/example/chenjunfan/myapplication/SettingActivity.java
+++ app/src/main/java/com/example/chenjunfan/myapplication/SettingActivity.java
@@ -1,11 +1,9 @@
 package com.example.chenjunfan.myapplication;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 /**
  * Created by chenjunfan on 16/7/10.
@@ -13,8 +11,6 @@ import android.widget.TextView;
 public class SettingActivity extends Activity {
 
     private ImageView imageBack;
-    private TextView changePasswd;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,19 +18,11 @@ public class SettingActivity extends Activity {
         setContentView(R.layout.activity_setting);
 
         imageBack = (ImageView) findViewById(R.id.img_back);
-        changePasswd = (TextView) findViewById(R.id.Changepasswd);
 
         imageBack.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
                 finish();
-            }
-        });
-        changePasswd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SettingActivity.this, ChangepasswdActivity.class);
-                startActivity(intent);
             }
         });
     }
